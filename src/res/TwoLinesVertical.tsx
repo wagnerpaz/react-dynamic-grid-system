ae9b1afca8d536766b67f7ca27d10224d8ce7f5b
Wagner de Oliveira Paz <wagner.o.paz@gmail.com>
2021-04-14 15:32:57 -0300

    removed bounding div from three dots

--- src/res/TwoLinesVertical.tsx
+++ src/res/TwoLinesVertical.tsx
@@ -2,28 +2,31 @@ import React from 'react';
 
 type Props = {
   color?: string;
-  size?: number;
+  width?: number;
+  height?: number;
   horizontal?: boolean;
 };
 
-const ThreeDotsVertical = ({
-  size = 16,
+const TwoLinesVertical = ({
+  width = 16,
+  height = 16,
   horizontal = false,
   color = '#000'
 }: Props) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      width={size}
-      height={size}
+      width={width}
+      height={height}
       fill={color}
       className='bi bi-three-dots-vertical'
-      viewBox={`0 0 ${size} ${size}`}
+      viewBox={`0 0 ${width} ${height}`}
       transform={horizontal ? 'rotate(90)' : undefined}
     >
-      <path d='M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z' />
+      <line x1='0' y1='0' x2='0' y2={height} stroke={color} />
+      <line x1={width} y1='0' x2={width} y2={height} stroke={color} />
     </svg>
   );
 };
 
-export default ThreeDotsVertical;
+export default TwoLinesVertical;
